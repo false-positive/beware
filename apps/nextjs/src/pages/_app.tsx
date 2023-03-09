87a3283ea52c21dba15f32a7e4f04de32828f74f
Kerzoken <78972321+Kerzoken@users.noreply.github.com>
2023-03-09 15:30:14 +0200

    Changed global font

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -1,18 +1,23 @@
 import "../styles/main.scss";
 import type { AppType } from "next/app";
+import localFont from "next/font/local";
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 import { api } from "~/utils/api";
+
+const myFont = localFont({ src: "../../public/fonts/Colfax-Medium.ttf" });
 
 const MyApp: AppType<{ session: Session | null }> = ({
     Component,
     pageProps: { session, ...pageProps },
 }) => {
     return (
-        <SessionProvider session={session}>
-            <Component {...pageProps} />
-        </SessionProvider>
+        <main className={myFont.className}>
+            <SessionProvider session={session}>
+                <Component {...pageProps} />
+            </SessionProvider>
+        </main>
     );
 };
 
