ce6de4ce682af3120463235451c954280c042733
Kerzoken <78972321+Kerzoken@users.noreply.github.com>
2023-03-09 11:30:16 +0200

    Setuped 7-1 SASS Architecture

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "../styles/main.scss";
 import type { AppType } from "next/app";
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
@@ -6,14 +6,14 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
+    Component,
+    pageProps: { session, ...pageProps },
 }) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+    return (
+        <SessionProvider session={session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
 };
 
 export default api.withTRPC(MyApp);
