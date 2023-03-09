81ce91d017cb64dfcc985b03ad78324f31a739c9
bvpav <78971592+bvpav@users.noreply.github.com>
2023-03-09 16:37:03 +0200

    Migrated part of it to TS

--- apps/docker-orchestry/src/spawn-docker.ts
+++ apps/docker-orchestry/src/spawn-docker.ts
@@ -1,4 +1,4 @@
-const { exec } = require("node:child_process");
+import { exec } from "node:child_process";
 
 export const defaultOptions = {
     name: "webtop-test",
@@ -9,28 +9,32 @@ export const defaultOptions = {
     appdata: "/mnt/user/appdata/webtop-test",
 };
 
-function dockerString(options) {
+export type Options = typeof defaultOptions;
+
+function dockerString(options: Options) {
     return `docker run -d --name ${options.name} -p ${options.webPort}:3000 -e PUID=${options.puid} -e PGID=${options.pgid} -e TZ=${options.timezone} -v ${options.appdata}/config:/config -v ${options.appdata}/data:/data -v ${options.appdata}/logs:/logs --restart unless-stopped linuxserver/webtop`;
 }
 
-function fakeDockerString(options) {
+function fakeDockerString(options: Options) {
     const { pid } = exec("python -m http.server ${options.webPort}");
     return `echo ${pid}`;
 }
 
-const createDockerContainerFactory = (cmdFunction) => (options) => {
-    return new Promise((resolve, reject) =>
-        exec(cmdFunction(options), (err, stdout, stderr) => {
-            if (err) {
-                reject(err);
-            }
-            resolve(stdout);
-        }),
-    );
-};
+const createDockerContainerFactory =
+    (cmdFunction: (options: Options) => string) => (options: Options) => {
+        return new Promise((resolve, reject) =>
+            exec(cmdFunction(options), (err, stdout, stderr) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(stdout);
+            }),
+        );
+    };
 
-export function createDockerContainer(options) {
+export function createDockerContainer(options: Options) {
     // HACK: made to test in bozhidar's computer
+    // eslint-disable-next-line turbo/no-undeclared-env-vars
     if (process.env.USER !== "bozhidar") {
         return createDockerContainerFactory(dockerString)(options);
     } else {
