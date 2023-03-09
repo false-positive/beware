bb606d4dafe454e437dc783ed83ee808e497b7ce
Kaloyan <kaloyanmiladinov@gmail.com>
2023-03-09 15:30:41 +0200

    Added basic method for creating docker container

---
+++ apps/docker_orchestry/spawn_docker.js
@@ -0,0 +1,24 @@
+const { exec } = require("node:child_process");
+
+const options = {
+    name: "webtop-test",
+    webPort: "5000",
+    puid: "1000",
+    pgid: "1000",
+    timezone: "Europe/Sofia",
+    appdata: "/mnt/user/appdata/webtop-test",
+};
+
+function dockerString(options) {
+    return `docker run -d --name ${options.name} -p ${options.webPort}:3000 -e PUID=${options.puid} -e PGID=${options.pgid} -e TZ=${options.timezone} -v ${options.appdata}/config:/config -v ${options.appdata}/data:/data -v ${options.appdata}/logs:/logs --restart unless-stopped linuxserver/webtop`;
+}
+
+export function createDockerContainer(options) {
+    exec(dockerString(options), (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(stdout);
+    });
+}
