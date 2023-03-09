58cb83933b8950acc282e7467dd0164e93e2cab5
bvpav <78971592+bvpav@users.noreply.github.com>
2023-03-09 16:12:59 +0200

    Setup testing on my pc

--- apps/docker_orchestry/spawn_docker.js
+++ apps/docker_orchestry/spawn_docker.js
@@ -13,13 +13,26 @@ function dockerString(options) {
     return `docker run -d --name ${options.name} -p ${options.webPort}:3000 -e PUID=${options.puid} -e PGID=${options.pgid} -e TZ=${options.timezone} -v ${options.appdata}/config:/config -v ${options.appdata}/data:/data -v ${options.appdata}/logs:/logs --restart unless-stopped linuxserver/webtop`;
 }
 
-export function createDockerContainer(options) {
+function fakeDockerString(options) {
+    return "python -m http.server ${options.webPort}";
+}
+
+const createDockerContainerFactory = (cmdFunction) => (options) => {
     return new Promise((resolve, reject) =>
-        exec(dockerString(options), (err, stdout, stderr) => {
+        exec(cmdFunction(options), (err, stdout, stderr) => {
             if (err) {
                 reject(err);
             }
             resolve(stdout);
         }),
     );
+};
+
+export function createDockerContainer(options) {
+    // HACK: made to test in bozhidar's computer
+    if (process.env.USER !== "bozhidar") {
+        return createDockerContainerFactory(dockerString)(options);
+    } else {
+        return createDockerContainerFactory(fakeDockerString)(options);
+    }
 }
