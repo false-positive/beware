9eb35de2e26008232646beee0fd5b9df25927584
Kerzoken <78972321+Kerzoken@users.noreply.github.com>
2023-03-09 15:19:20 +0200

    Created layout for upper part of Course Description page

--- apps/nextjs/src/pages/course/[id].tsx
+++ apps/nextjs/src/pages/course/[id].tsx
@@ -7,10 +7,13 @@ const CourseDetail = () => {
                 <Header></Header>
                 <h1 className="heading center-text">Course Title</h1>
                 <div className="course-info">
-                    <div className="course-info_progress">
-                        <div className="course-info__chart"></div>
-                        <span className="course-info__count">70%</span>
+                    <div className="course-info__progress ">
+                        <div className="pie">70%</div>
                     </div>
+
+                    {/* <div className="course-info__chart">
+                            <span className="course-info__count">70%</span>
+                        </div> */}
                     <p className="course-info__description">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Quibusdam illo distinctio nam esse tempore. Ab magnam
@@ -18,6 +21,7 @@ const CourseDetail = () => {
                         vero dignissimos aperiam animi, vel repudiandae!
                     </p>
                 </div>
+                {/* <div className="pie">Timeline</div> */}
                 <div className="timeline">Timeline</div>
             </main>
         </>
