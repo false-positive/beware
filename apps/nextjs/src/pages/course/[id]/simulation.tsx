1177eecb3bc2c4c91cca845e8dc745e2ed1ccbb1
bvpav <bvpav@users.noreply.github.com>
2023-03-11 00:57:33 +0200

    Setup simulation flow

--- apps/nextjs/src/pages/course/[id]/simulation.tsx
+++ apps/nextjs/src/pages/course/[id]/simulation.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
@@ -9,12 +10,28 @@ const Intro = () => {
     const id = useRouter().query.id as string;
     useSession({ required: true });
     const { data: course } = api.course.byId.useQuery({ id });
+    const utils = api.useContext();
+    const [error, setError] = useState(false);
+    const { mutate: checkAnswer, isLoading } = api.course.answer.useMutation({
+        onSuccess: (isCorrect) => {
+            // refetch the course to get the updated answer
+            void utils.course.byId.invalidate({ id });
+            if (!isCorrect) {
+                setError(true);
+            } else {
+                setError(false);
+            }
+        },
+        onError: (err) => {
+            setError(true);
+        },
+    });
 
-    console.log(process.env.NEXT_PUBLIC_DOCKER_HOST);
-    // console.log(course.user.machinePort);
     if (course == null) {
         return null;
     }
+
+    console.log(error);
 
     return (
         <main className="page-course-intro">
@@ -26,37 +43,65 @@ const Intro = () => {
                 <div className="simulation">
                     <div className="simulation__tasks">
                         <h2>Tasks</h2>
-                        <div className="question">
-                            <p className="question__text">1. Question 1</p>
-                            <p className="question__feedback--success">
-                                Wrong answer
-                            </p>
-                            <form action="#">
-                                <input type="text" />
-                                <button>Check</button>
-                            </form>
-                        </div>
-                        <div className="question">
-                            <p>1. Question 1</p>
-                            <form action="#">
-                                <input type="text" />
-                                <button>Check</button>
-                            </form>
-                        </div>
-                        <div className="question">
-                            <p>1. Question 1</p>
-                            <form action="#">
-                                <input type="text" />
-                                <button>Check</button>
-                            </form>
-                        </div>
-                        <div className="question">
-                            <p>1. Question 1</p>
-                            <form action="#">
-                                <input type="text" />
-                                <button>Check</button>
-                            </form>
-                        </div>
+                        {course.questions.map((question, index) => (
+                            <div
+                                className={`question ${
+                                    index == course.lastAnsweredQuestionOrder &&
+                                    !isLoading
+                                        ? "question--active"
+                                        : ""
+                                }`}
+                                key={question.id}
+                            >
+                                {/* <p className="question__feedback question__feedback--error question__feedback--invisible">
+                                    Wrong answer. Please try again.
+                                </p> */}
+                                <p className="question__text">
+                                    {index + 1}. {question.instruction}
+                                </p>
+                                <form
+                                    action="#"
+                                    onSubmit={(e) => {
+                                        e.preventDefault();
+                                        const data = new FormData(
+                                            e.target as HTMLFormElement,
+                                        );
+                                        const answer = data.get("answer");
+                                        if (answer === null) {
+                                            return;
+                                        }
+                                        // console.log(question.id);
+                                        checkAnswer({
+                                            questionId: question.id,
+                                            answer: answer.toString(),
+                                        });
+                                    }}
+                                >
+                                    {question.answer ? (
+                                        <input
+                                            type="text"
+                                            className="question__input"
+                                            // placeholder={question.answer}
+                                            value={question.answer}
+                                            readOnly
+                                        />
+                                    ) : (
+                                        <input
+                                            type="text"
+                                            name="answer"
+                                            className="question__input"
+                                            placeholder="VBA Bank"
+                                        />
+                                    )}
+                                    <button
+                                        className="question__submit"
+                                        disabled={isLoading}
+                                    >
+                                        Check
+                                    </button>
+                                </form>
+                            </div>
+                        ))}
                     </div>
                     <div className="simulation__display">
                         <iframe
@@ -66,12 +111,15 @@ const Intro = () => {
                         ></iframe>
                     </div>
                 </div>
-                <Link
-                    href={`/course/${id}/summary`}
-                    className="page-course-intro__button btn"
-                >
-                    Continue
-                </Link>
+                {course.lastAnsweredQuestionOrder ===
+                    course.questions.length && (
+                    <Link
+                        href={`/course/${id}/summary`}
+                        className="page-course-intro__button btn"
+                    >
+                        Continue
+                    </Link>
+                )}
             </div>
         </main>
     );
