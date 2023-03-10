1b5425aada3deb17081fa50f4635c276353f5c1e
bvpav <78971592+bvpav@users.noreply.github.com>
2023-03-10 13:53:42 +0200

    Fixed DOM structure

--- apps/nextjs/src/pages/home.tsx
+++ apps/nextjs/src/pages/home.tsx
@@ -14,52 +14,28 @@ const Home = () => {
             </div>
             <section>
                 <h1 className="heading ">Home</h1>
-                <div className="course-cards">
-                    {courses?.map((course) => (
-                        <div key={course.id} className="course-card">
-                            <h2 className="course-card__title">
-                                {course.name}
-                            </h2>
-                            <div className="progress-bar">
-                                <div className="progress-bar__bar">
-                                    <div
-                                        className="progress-bar__colored"
-                                        style={{
-                                            width: `${30}%`,
-                                        }}
-                                    >
-                                        &nbsp;
-                                    </div>
-                                </div>
-                                <p className="progress-bar__count">{30}%</p>
-                            </div>
-                        </div>
-                    ))}
-                </div>
                 <div className="home__content">
                     <div className="course-cards">
-                        <div className="course-card">
-                            <h2 className="course-card__title">Title</h2>
-                            <div className="progress-bar">
-                                <div className="progress-bar__bar">
-                                    <div className="progress-bar__colored">
-                                        &nbsp;
+                        {courses?.map((course) => (
+                            <div key={course.id} className="course-card">
+                                <h2 className="course-card__title">
+                                    {course.name}
+                                </h2>
+                                <div className="progress-bar">
+                                    <div className="progress-bar__bar">
+                                        <div
+                                            className="progress-bar__colored"
+                                            style={{
+                                                width: `${30}%`,
+                                            }}
+                                        >
+                                            &nbsp;
+                                        </div>
                                     </div>
+                                    <p className="progress-bar__count">{30}%</p>
                                 </div>
-                                <p className="progress-bar__count">10%</p>
                             </div>
-                        </div>
-                        <div className="course-card">
-                            <h2 className="course-card__title">Title</h2>
-                            <div className="progress-bar">
-                                <div className="progress-bar__bar">
-                                    <div className="progress-bar__colored">
-                                        &nbsp;
-                                    </div>
-                                </div>
-                                <p className="progress-bar__count">20%</p>
-                            </div>
-                        </div>
+                        ))}
                     </div>
                     <div className="home__image">
                         <SecurityPicture />
