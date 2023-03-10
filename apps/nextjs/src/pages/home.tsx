1c9e44ed67db976ccf93284e1562ac6f8d7a0da7
nikolasachkov <nikola.sachkov@gmail.com>
2023-03-10 12:37:09 +0200

    added svg image to home page

--- apps/nextjs/src/pages/home.tsx
+++ apps/nextjs/src/pages/home.tsx
@@ -1,5 +1,7 @@
 // import Image from "next/image";
+
 import Header from "../components/header";
+import SecurityPicture from "../components/home_page_security_picture";
 
 const Home = () => {
     return (
@@ -9,28 +11,33 @@ const Home = () => {
             </div>
             <section>
                 <h1 className="heading ">Home</h1>
-                <div className="course-cards">
-                    <div className="course-card">
-                        <h2 className="course-card__title">Title</h2>
-                        <div className="progress-bar">
-                            <div className="progress-bar__bar">
-                                <div className="progress-bar__colored">
-                                    &nbsp;
+                <div className="home__content">
+                    <div className="course-cards">
+                        <div className="course-card">
+                            <h2 className="course-card__title">Title</h2>
+                            <div className="progress-bar">
+                                <div className="progress-bar__bar">
+                                    <div className="progress-bar__colored">
+                                        &nbsp;
+                                    </div>
                                 </div>
+                                <p className="progress-bar__count">10%</p>
                             </div>
-                            <p className="progress-bar__count">10%</p>
+                        </div>
+                        <div className="course-card">
+                            <h2 className="course-card__title">Title</h2>
+                            <div className="progress-bar">
+                                <div className="progress-bar__bar">
+                                    <div className="progress-bar__colored">
+                                        &nbsp;
+                                    </div>
+                                </div>
+                                <p className="progress-bar__count">20%</p>
+                            </div>
                         </div>
                     </div>
-                    <div className="course-card">
-                        <h2 className="course-card__title">Title</h2>
-                        <div className="progress-bar">
-                            <div className="progress-bar__bar">
-                                <div className="progress-bar__colored">
-                                    &nbsp;
-                                </div>
-                            </div>
-                            <p className="progress-bar__count">20%</p>
-                        </div>
+                    <div className="home__image">
+                        <SecurityPicture />
                     </div>
                 </div>
             </section>
