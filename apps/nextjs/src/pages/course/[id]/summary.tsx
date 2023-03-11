6f7f67097bb430cf31f5f1a76103744f784e1869
bvpav <78971592+bvpav@users.noreply.github.com>
2023-03-11 02:33:31 +0200

    Removed dummy txt

--- apps/nextjs/src/pages/course/[id]/summary.tsx
+++ apps/nextjs/src/pages/course/[id]/summary.tsx
@@ -2,12 +2,19 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 
+import { api } from "~/utils/api";
+import { ColfaxLight } from "~/utils/fonts";
 import Header from "../../../components/header";
 
 const Summary = () => {
-    const router = useRouter();
     const id = useRouter().query.id as string;
     useSession({ required: true });
+
+    const { data: course } = api.course.byId.useQuery({ id });
+
+    if (course === undefined) {
+        return null;
+    }
 
     return (
         <main className="page-course-intro">
@@ -17,54 +24,14 @@ const Summary = () => {
                     Summary
                 </h1>
 
-                <p className="course-info__description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Dolorum dolor aperiam qui amet, aut repellendus sunt autem
-                    sit praesentium beatae facilis quis atque optio nesciunt
-                    quam modi
-                    <br />
-                    <br />
-                    temporibus exercitationem nulla! <br />
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Rem autem delectus cupiditate itaque, magnam optio officia
-                    enim error saepe perferendis fuga tenetur dolores cum
-                    doloremque praesentium eum nesciunt, nostrum mollitia.
-                    <br />
-                    <br />
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Rem autem delectus cupiditate itaque, magnam optio officia
-                    enim error saepe perferendis fuga tenetur dolores cum
-                    doloremque praesentium eum nesciunt, nostrum mollitia.
-                    <br />
-                    <br />
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Rem autem delectus cupiditate itaque, magnam optio officia
-                    enim error saepe perferendis fuga tenetur dolores cum
-                    doloremque praesentium eum nesciunt, nostrum mollitia.
-                    <br />
-                    <br />
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Rem autem delectus cupiditate itaque, magnam optio officia
-                    enim error saepe perferendis fuga tenetur dolores cum
-                    doloremque praesentium eum nesciunt, nostrum mollitia.
-                    <br />
-                    <br />
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Rem autem delectus cupiditate itaque, magnam optio officia
-                    enim error saepe perferendis fuga tenetur dolores cum
-                    doloremque praesentium eum nesciunt, nostrum mollitia.
-                    <br />
-                    <br />
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Rem autem delectus cupiditate itaque, magnam optio officia
-                    enim error saepe perferendis fuga tenetur dolores cum
-                    doloremque praesentium eum nesciunt, nostrum mollitia.
-                    <br />
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Rem autem delectus cupiditate itaque, magnam optio officia
-                    enim error saepe perferendis fuga tenetur dolores cum
-                    doloremque praesentium eum nesciunt, nostrum mollitia.
-                </p>
+                {course.description.split("\n").map((p, i) => (
+                    <p
+                        key={i}
+                        className={`course-info__description ${ColfaxLight.className}`}
+                    >
+                        {p}
+                    </p>
+                ))}
 
                 <Link href={`/home`} className="page-course-intro__button btn">
                     Finish!
