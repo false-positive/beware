a39c29ceeae903a46fbe633e6fbe6689277d61d2
bvpav <78971592+bvpav@users.noreply.github.com>
2023-05-17 10:13:23 +0300

    Made machine router kill containers
    This fixes the Vercel FUNCTION_INVOCATION_TIMEOUT error.

--- packages/api/src/router/machine.router.ts
+++ packages/api/src/router/machine.router.ts
@@ -133,8 +133,7 @@ export const machineRouter = createTRPCRouter({
             }
 
             const container = ctx.docker.container.get(usrCourse.machineId);
-            // XXX: maybe just kill here to make it faster?
-            await container.stop();
+            await container.kill();
             await container.delete();
             await ctx.prisma.userCourse.update({
                 where: {
